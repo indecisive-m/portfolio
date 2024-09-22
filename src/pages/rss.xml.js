5ec1cb2a571ebebfaddc1fa7f01e79fd0abb3c12
indecisive-m <michr.watkins@btinternet.com>
2024-09-22 21:13:40 +0100

    resolved typescript issues

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,13 +2,13 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const posts = getCollection("blog");
+  const posts = await getCollection("blog");
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     items: posts.map((post) => ({
       title: post.data.title,
-      publishDate: post.data.publishDate,
+      pubDate: post.data.publishDate,
       description: post.data.description,
       link: `/posts/${post.slug}/`,
     })),
