09a1b9adec4548c9cf75b4c54a28ae70b8099b2a
indecisive-m <michr.watkins@btinternet.com>
2024-09-25 22:30:04 +0100

    small edits

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ import { getCollection } from "astro:content";
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: "Astro Learner | Blog",
-    description: "My journey learning Astro",
+    title: "Mike Watkins' Blog",
+    description: "My journey learning to code.",
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.publishDate,
