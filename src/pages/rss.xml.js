e49ffdfda67a38f1ea1e03d3e8c13290f2dc5f39
indecisive-m <michr.watkins@btinternet.com>
2025-02-09 18:23:12 +0000

    More work on the rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,9 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("blog");
@@ -20,9 +24,11 @@ export async function GET(context) {
       pubDate: post.data.publishDate,
       description: post.data.description,
       link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
     })),
     site: context.site,
-    customData: `<language>en-us</language>`,
     stylesheet: "/pretty-feed-v3.xsl",
   });
 }
